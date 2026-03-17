8aea42224ea4cf04d91081f025991ad464d4f72b
Stanisław Kuryś <s34270@pjwstk.edu.pl>
2026-03-17 19:14:34 +0100

    Initial project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = {3, 5, 12, 7, 10};
+
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+    }
+}
