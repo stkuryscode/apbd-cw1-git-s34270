8b597f3d9b1fe7cc76bd8b54f14cbd4288ba3704
Stanisław Kuryś <s34270@pjwstk.edu.pl>
2026-03-17 20:01:19 +0100

    dodana metoda calculateAverage

--- src/StatisticsHelper.java
+++ src/StatisticsHelper.java
@@ -1,3 +1,13 @@
+import java.util.Arrays;
+
 public class StatisticsHelper {
+
+    public static double calculateAverage(int[] values) {
+        if (values == null || values.length == 0) {
+            return 0.0;
+        }
+
+        return Arrays.stream(values).average().orElse(0.0);
+    }
 
 }
